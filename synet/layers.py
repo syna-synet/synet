2720f3e897cd00c04be982b5b7dc0b9400bcf86a
rbezen <ran.bezen@synaptics.com>
2023-07-27 14:58:15 +0300

    Add DepthwiseSeparableCov, DepthwiseSeparableInvertedResidual, and traditional InvertedResidual

--- synet/layers.py
+++ synet/layers.py
@@ -3,12 +3,15 @@
 - base.py should only import from torch, keras, and tensorflow.
 - layers.py should only import from base.py.
 - [chip].py should only import from base.py and layers.py."""
+from typing import Union, Tuple, Optional
 
-from .base import ReLU, BatchNorm, Conv2d, askeras, Module, Sequential
+from torch import Tensor
+
+from .base import (ReLU, BatchNorm, Conv2d, Module, Sequential,
+                   DepthwiseConv2d)
 
 
-from .base import BatchNorm
-class InvertedResidual(Module):
+class Conv2dInvertedResidual(Module):
     """Inverted Resudual blocks are the main building block of
 MobileNet.  It is stable and gives low peek memory before and after.
 Additionally, the computations are extremely efficient on our chips
@@ -66,3 +69,103 @@ out of that layer.  num (default 4) convolutions are used in total.
 
     def forward(self, x):
         return self.model(x)
+
+
+class DepthwiseSeparableConv2D(Module):
+    """
+    Depthwise Separable convolution using pytorch
+    """
+
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int,
+                 kernel_size: Union[int, Tuple[int, int]],
+                 stride: int = 1,
+                 padding: Optional[bool] = True,
+                 bias: bool = False):
+        """
+
+        :param in_channels: Number of channels in the input
+        :param out_channels: Number of channels produced by the convolution
+        :param kernel_size: Size of the convolving kernel
+        :param conv_2d_params: Additional arguments passed to nn.Conv2d
+        """
+        super().__init__()
+        self.depthwise = DepthwiseConv2d(
+            in_channels, in_channels, kernel_size=kernel_size, stride=stride,
+            groups=in_channels, bias=False,
+            padding=padding)
+        self.pointwise = Conv2d(in_channels, out_channels,
+                                kernel_size=1, bias=bias)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.depthwise(x)
+        x = self.pointwise(x)
+        return x
+
+
+class DepthwiseInvertedResidual(Conv2dInvertedResidual):
+    """Inverted Resudual blocks are the main building block of
+        MobileNet.  It is stable and gives low peek memory before and after.
+        Additionally, the computations are extremely efficient on our chips
+
+    """
+
+    def __init__(self, in_channels, expansion_factor,
+                 out_channels=None, stride=1):
+        """
+        This inverted residual takes in_channels to
+         in_channels*expansion_factor with a 3x3 convolution.
+         Then after a batchnorm and ReLU, the activations are taken back down
+          to in_channels (or out_channels, if specified).  If out_channels is
+          not specified (or equals in_channels), and the stride is 1, then the
+          input will be added to the output before returning.
+        """
+        super().__init__(in_channels, expansion_factor, out_channels, stride)
+        if out_channels is None:
+            out_channels = in_channels
+        hidden = int(in_channels * expansion_factor)
+        pointwise_expand = Conv2d(
+            in_channels=in_channels, out_channels=hidden, kernel_size=1,
+            padding=False)
+        depthwise = DepthwiseConv2d(
+            hidden, hidden, kernel_size=3, groups=hidden, bias=False)
+        pointwise_squeeze = Conv2d(
+            in_channels=hidden, out_channels=out_channels, kernel_size=1,
+            padding=False)
+
+        self.layers = Sequential([
+            pointwise_expand,
+            BatchNorm(hidden),
+            ReLU(6),
+            depthwise,
+            ReLU(6),
+            BatchNorm(hidden),
+            pointwise_squeeze,
+            BatchNorm(out_channels)
+        ])
+
+
+class DepthwiseSeparableInvertedResidual(Conv2dInvertedResidual):
+    """
+    Kind of efficient (in aspect of work memory) InvertedResidual block.
+    """
+
+    def __init__(self, in_channels, expansion_factor,
+                 out_channels=None, stride=1):
+        super().__init__(in_channels, expansion_factor, out_channels, stride)
+        if out_channels is None:
+            out_channels = in_channels
+        hidden = int(in_channels * expansion_factor)
+        self.layers = Sequential([
+            DepthwiseSeparableConv2D(in_channels,
+                                     out_channels=hidden,
+                                     kernel_size=3,
+                                     stride=stride),
+            BatchNorm(hidden),
+            ReLU(6),
+            Conv2d(in_channels=hidden,
+                   out_channels=out_channels,
+                   kernel_size=1),
+            BatchNorm(out_channels)
+        ])
--- synet/sabre.py
+++ synet/sabre.py
@@ -1,4 +1,4 @@
 """katana.py includes imports which are compatible with Katana, and layer definitions that are only compatible with Katana.  However, Katana's capabilities are currently a subset of all other chip's capabilities, so it includes only imports for now."""
 
-from .layers import InvertedResidual, Head
+from .layers import Conv2dInvertedResidual, Head
 from .base import askeras, Conv2d, Cat, ReLU, BatchNorm, Grayscale
