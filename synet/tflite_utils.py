3264ca55a10dab447d66d25e049741a98ad79c43
Zacchaeus Scheffer <zscheffe@synaptics.com>
2024-02-20 18:39:42 -0800

    added: synet tflite_utils

--- synet/tflite_utils.py
+++ synet/tflite_utils.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """This module exists to hold all tflite related processing.  The main
 benefit of keeping this in a seperate modules is so that large
 dependencies (like ultralytics) need not be imported when simulating
@@ -8,21 +9,37 @@ however, do read any "Notes" sections in any function docstrings
 
 """
 
+from argparse import ArgumentParser
 from typing import Optional, List
 
-from cv2 import imread
+from cv2 import imread, rectangle, addWeighted, imwrite, resize
 from numpy import (newaxis, ndarray, int8, float32 as npfloat32,
                    concatenate as cat, max as npmax, argmax, moveaxis)
 from tensorflow import lite
-from torch import tensor, float32 as torchfloat32
+from torch import tensor, float32 as torchfloat32, sigmoid, tensordot, \
+    repeat_interleave
 from torchvision.ops import nms
 
 
-def tf_run(tflite, img, conf_thresh, iou_thresh, backend, task):
+def parse_opt(args=None):
+    parser = ArgumentParser()
+    parser.add_argument('tflite')
+    parser.add_argument('img')
+    parser.add_argument('--conf-thresh', type=float, default=.25)
+    parser.add_argument('--iou-thresh', type=float, default=.7)
+    parser.add_argument('--backend', default='ultralytics')
+    parser.add_argument('--task', default='segment')
+    parser.add_argument('--image-shape', nargs=2, type=int, default=None)
+    return parser.parse_args(args=args)
+
+
+def tf_run(tflite, img, conf_thresh=.25, iou_thresh=.7,
+           backend='ultralytics', task='segment', image_shape=None):
     """Run a tflite model on an image, including post-processing.
 
     Loads the tflite, loads the image, preprocesses the image,
     evaluates the tflite on the pre-processed image, and performs
+
     post-processing on the tflite output with a given confidence and
     iou threshold.
 
@@ -61,6 +78,9 @@ def tf_run(tflite, img, conf_thresh, iou_thresh, backend, task):
     # read the image if given as path
     if isinstance(img, str):
         img = imread(img)
+
+    if image_shape is not None:
+        img = resize(img, image_shape[::-1])
 
     # make image RGB (not BGR) channel order, BCHW dimensions, and
     # in the range [0, 1].  cv2's imread reads in BGR channel
@@ -179,7 +199,7 @@ def concat_reshape(model_output: List[ndarray],
     assert num_classes == 1, "apply_nms() hardcodes for num_classes=1"
     # obtain class confidences
     conf = npmax(cls, axis=1, keepdims=True),
-    if classes_to_index: # important for multi-class
+    if classes_to_index:  # important for multi-class
         conf = (*conf, argmax(cls, axis=1, keepdims=True))
 
     # possibly convert to xywh if desired
@@ -258,3 +278,52 @@ def apply_nms(preds: ndarray, conf_thresh: float, iou_thresh: float):
     # https://pytorch.org/vision/stable/generated/torchvision.ops.nms.html
     preds = preds[nms(preds[:, :4], preds[:, 4], iou_thresh)]
     return (preds, tensor(proto)) if is_tuple else preds
+
+
+def build_masks(preds, proto):
+    # contract mask embeddings with proto
+    #                      (  N x k   dot   k x h x w  )
+    masks = sigmoid(tensordot(preds[:, 6:], proto, dims=1))
+    # upsamle mask
+    for dim in 1, 2:
+        masks = repeat_interleave(masks, repeats=8, dim=dim)
+    # clip mask to box
+    for (x1, y1, x2, y2), mask in zip(preds[:, :4], masks):
+        # integer math may be off-by-one near boarder in this
+        # application.
+        mask[:int(y1)] = 0
+        mask[int(y2):] = 0
+        mask[:, :int(x1)] = 0
+        mask[:, int(x2):] = 0
+    return preds[:, :6], masks
+
+
+def main(args=None):
+    # read options, run model
+    opt = parse_opt(args)
+    opt.img = imread(opt.img)
+    if opt.image_shape is not None:
+        opt.img = resize(opt.img, opt.image_shape[::-1])
+        opt.image_shape = None
+    preds, masks = build_masks(*tf_run(**vars(opt)))
+
+    # shrink mask if upsamle gave too large of area
+    for imdim, maskdim in (0, 1), (1, 2):
+        extra, carry = divmod(masks.shape[maskdim] - opt.img.shape[imdim], 2)
+        if extra == carry == 0:
+            continue
+        masks = masks[(*(slice(None),)*maskdim, slice(extra, -(extra+carry)))]
+
+    # visualize masks and rectangles
+    img_overlay = opt.img.copy()
+    img_overlay[masks.max(0).values > .5] = (0, 0, 255)
+    img = addWeighted(opt.img, .5, img_overlay, .5, 0)
+    for x1, y1, x2, y2, *cls in preds:
+        rectangle(img, (int(x1), int(y1)),
+                       (int(x2), int(y2)),
+                  (0, 0, 255), 2)
+    imwrite('output.png', img)
+
+
+if __name__ == '__main__':
+    main()
