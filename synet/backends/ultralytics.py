8bb0fc36303f8bb1aac8b920a6a06b09a4d0d5cd
Zacchaeus <eikcaz@zacchae.us>
2025-08-27 11:45:50 -0700

    fix ultralytics segment visualization

--- synet/backends/ultralytics.py
+++ synet/backends/ultralytics.py
@@ -344,9 +344,11 @@ get_backend('ultralytics').patch()
                                                    xywh=True)
                             if isinstance(preds, tuple):
                                 preds = (tensor(preds[0][None])
-                                         .permute(0, 2, 1),
+                                         .permute(0, 2, 1)
+                                         .contiguous(),
                                          tensor(preds[1][None])
-                                         .permute(0, 2, 3, 1))
+                                         .permute(0, 2, 3, 1)
+                                         .contiguous())
                             else:
                                 preds = tensor(preds[None]).permute(0, 2, 1)
                             return super().postprocess(preds, *args, **kwds)
